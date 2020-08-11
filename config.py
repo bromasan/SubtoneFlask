5e06e56a73380e73e0f34712cbf7ccf1b1f8c949
Brock Guzman <brock@Brocks-MBP.home>
2020-08-10 23:06:35 -0400

    initial commit for heroku

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        "postgresql://bromasan:s$i4+P!@localhost:5432/subtone"
+        "postgres://mqoqrgevidbckm:3493a12831192165d630b545b22a6d9bcb3b34da85b99f7ac8b5f103fbc098ff@ec2-52-86-116-94.compute-1.amazonaws.com:5432/danqhipjt3o754"
     DEBUG = True
